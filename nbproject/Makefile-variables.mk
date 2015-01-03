67f137cf48d29ec39a3ffe7dfc2b501c07d45c9a
Beke András <beke.andras.mkolc@gmail.com>
2015-01-03 11:15:32 +0100

    Initial with add nbproject

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FLOWMETER.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FLOWMETER.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=flowmeter.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/flowmeter.tar
